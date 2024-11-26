734d46a0504e360b63dde8b3c940ca7070eff7e7
Sérgio Oliveira <sergiowallace11@hotmail.com>
2024-11-26 16:53:49 +0000

    improving project.

--- script.js
+++ script.js
@@ -1,54 +1,44 @@
+const result = document.querySelector(".result");
+const humanScore = document.querySelector("#human-score");
+const machineScore = document.querySelector("#machine-score");
 
-
-
-const result = document.querySelector('.result')
-const humanScore = document.querySelector('#human-score')
-const machineScore = document.querySelector('#machine-score')
-
-
-//ENUM → para guardar palavras em repetição no código e evitar erros de digitação.
 const GAME_CHOICES = {
-    ROCK: "rock",
-    PAPER: "paper",
-    SCISSORS: "scissors"
-}
+  ROCK: "rock",
+  PAPER: "paper",
+  SCISSORS: "scissors",
+};
 
-/* 
-humanScoreNumber → Camel Case
-GAME_OPTIONS → Snake Case 
-*/
 let scoreHumanNumber = 0;
 let scoreMachineNumber = 0;
 
 const playHuman = (humanChoices) => {
-    playTheGame(humanChoices, playMachine())
-}
+  playTheGame(humanChoices, playMachine());
+};
 
 const playMachine = () => {
-    const choices = [GAME_CHOICES.ROCK, GAME_CHOICES.PAPER, GAME_CHOICES.SCISSORS]
-    const randomNumber = Math.floor(Math.random() * 3)
-    return choices [randomNumber]
-}
+  const choices = [
+    GAME_CHOICES.ROCK,
+    GAME_CHOICES.PAPER,
+    GAME_CHOICES.SCISSORS,
+  ];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+};
 
 const playTheGame = (human, machine) => {
-    if (human === machine) {
-        result.textContent = "Equality"
-    }
-
-    else if (human === GAME_CHOICES.PAPER && machine === GAME_CHOICES.ROCK ||
-        human === GAME_CHOICES.ROCK && machine === GAME_CHOICES.SCISSORS ||
-        human === GAME_CHOICES.SCISSORS && machine === GAME_CHOICES.PAPER) {
-        result.textContent = "You win!"
-        scoreHumanNumber++
-        humanScore.textContent = scoreHumanNumber
-
-    }
-    else {
-        scoreMachineNumber++
-        machineScore.textContent = scoreMachineNumber
-        result.textContent = "You lost to Alexa!"
-
-    }
-
-
-}
+  if (human === machine) {
+    result.textContent = "Equality";
+  } else if (
+    (human === GAME_CHOICES.PAPER && machine === GAME_CHOICES.ROCK) ||
+    (human === GAME_CHOICES.ROCK && machine === GAME_CHOICES.SCISSORS) ||
+    (human === GAME_CHOICES.SCISSORS && machine === GAME_CHOICES.PAPER)
+  ) {
+    result.textContent = "You win!";
+    scoreHumanNumber++;
+    humanScore.textContent = scoreHumanNumber;
+  } else {
+    scoreMachineNumber++;
+    machineScore.textContent = scoreMachineNumber;
+    result.textContent = "You lost to IA!";
+  }
+};
